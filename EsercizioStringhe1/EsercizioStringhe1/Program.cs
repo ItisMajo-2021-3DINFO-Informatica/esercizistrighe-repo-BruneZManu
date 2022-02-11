7483bc24bd78872db7041533f1865d3c6c613ad4
BruneZManu <98150791+BruneZManu@users.noreply.github.com>
2022-02-11 09:57:15 +0100

    commit

---
+++ EsercizioStringhe1/EsercizioStringhe1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EsercizioStringhe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sommac = -1;
+            int sommav = 0;
+            char iniziale ;  
+            Console.Write("Scrivi una parola, se vuoi smettere di scrivere parole scrivi fine, ti scrivo quante: ");
+            string parola="";
+            
+            while (parola != "fine")
+            {
+                parola = Console.ReadLine();
+                iniziale = parola[0];
+                if (iniziale == 'a')
+                {
+                    sommav = sommav + 1;
+                }
+                else if (iniziale == 'e')
+                {
+                    sommav = sommav + 1;
+                }
+                else if (iniziale == 'i')
+                {
+                    sommav = sommav + 1;
+                }
+                else if (iniziale == 'o')
+                {
+                    sommav = sommav + 1;
+                }
+                else if (iniziale == 'u')
+                {
+                    sommav = sommav + 1;
+                }
+                else 
+                {
+                    sommac = sommac + 1;
+                }
+            }
+            Console.WriteLine($"Sono presenti {sommav} parole che iniziano per vocale e sono presente {sommac} parole che iniziano per consonante.");
+        }
+    }
+}
---
+++ EsercizioStringhe2/EsercizioStringhe2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EsercizioStringhe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scrivi una serie di parole, scrivi fine quando desideri concludere;" +
+                " ti dico quante parole hanno meno di otto caratteri e quante più: ");
+            string parola="";
+            int menott = 0;
+            int piùott = 0;
+            
+            double nparole = 0;
+            double somma = 0;
+            while (parola != "fine")
+            {
+                parola = Console.ReadLine();
+                int lungh = parola.Length;
+                if (lungh >= 8)
+                {
+                    piùott = piùott + 1;
+                    nparole = nparole + 1;
+                    somma = somma + lungh;
+
+                }
+                else if(lungh < 8)
+                {
+                    menott = menott + 1;
+                    nparole = nparole + 1;
+                    somma = somma + lungh;
+                }
+            }
+            double media = somma / nparole;
+            Console.WriteLine($"Ci sono {piùott} parole con almeno otto caratteri e {menott} " +
+                $"parole con meno di otto e la media dei caratteri è {media}.");
+        }
+    }
+}
